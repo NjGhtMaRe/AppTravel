09420165fc625255fb47428a87f227bceef0491b
Andy <andy@MacBook-Pro.local>
2022-12-22 15:45:18 +0700

    Add attraction cards component

--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {FlatList, SafeAreaView, Text, View} from 'react-native';
 import Title from '../../components/Title';
 import styles from './styles';
 import jsonData from '../../data/attractions.json';
@@ -12,28 +12,42 @@ const Home = () => {
   const [categories, setCategories] = useState([ALL, ...categoriesData]);
   const [selectedCategory, setSelectedCategory] = useState(ALL);
 
+  useEffect(() => {
+    setData(jsonData);
+    console.log(data);
+  }, []);
+
   return (
     <SafeAreaView>
-      <View style={styles.headerContainer}>
-        <Title text={'Where do'} />
-        <Title text={'you want to go?'} style={{fontWeight: 'bold'}} />
-        <Title
-          text={'Explore Attractions'}
-          style={{
-            fontSize: 24,
-            color: 'black',
-            marginTop: 32,
-            marginBottom: 32,
-          }}
-        />
-        <View>
-          <Categories
-            categories={categories}
-            selectedCategory={selectedCategory}
-            onCategoryPress={setSelectedCategory}
-          />
-        </View>
-      </View>
+      <FlatList
+        data={data}
+        numColumns={2}
+        keyExtractor={item => String(item?.id)}
+        ListHeaderComponent={
+          <>
+            <View style={styles.headerContainer}>
+              <Title text={'Where do'} />
+              <Title text={'you want to go?'} style={{fontWeight: 'bold'}} />
+              <Title
+                text={'Explore Attractions'}
+                style={{
+                  fontSize: 24,
+                  color: 'black',
+                  marginTop: 32,
+                  marginBottom: 32,
+                }}
+              />
+              <View>
+                <Categories
+                  categories={categories}
+                  selectedCategory={selectedCategory}
+                  onCategoryPress={setSelectedCategory}
+                />
+              </View>
+            </View>
+          </>
+        }
+      />
     </SafeAreaView>
   );
 };
---
+++ src/components/AttractionCards/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AttractionCards = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default React.memo(AttractionCards);
