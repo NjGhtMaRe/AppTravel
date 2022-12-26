725eb849bd6cff407efe4a57e841a2add42dbae5
Andy <andy@MacBook-Pro.local>
2022-12-26 16:04:09 +0700

    Keep dev AttractionDetails

--- src/Screen/AttractionDetails/index.js
+++ src/Screen/AttractionDetails/index.js
@@ -6,11 +6,15 @@ import {
   Image,
   Pressable,
   View,
+  ScrollView,
 } from 'react-native';
 import styles from './styles';
 
 const AttractionDetails = ({navigation, route}) => {
   const {item} = route?.params || {};
+  const mainImage = item?.images?.length ? item?.images[0] : null;
+  const slicedImage = item?.images?.length ? item?.images.slice(0, 5) : [];
+  const diffImage = item?.images?.length - slicedImage?.length;
 
   const onBack = () => {
     navigation.goBack();
@@ -18,22 +22,46 @@ const AttractionDetails = ({navigation, route}) => {
   console.log(item);
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground style={styles.mainImage} source={{uri: item?.images[0]}}>
-        <View style={styles.headerContainer}>
-          <Pressable hitSlop={8} onPress={onBack}>
-            <Image
-              style={styles.iconBack}
-              source={require('../../assets/back.png')}
-            />
-          </Pressable>
-          <Pressable hitSlop={8}>
-            <Image
-              style={styles.iconShare}
-              source={require('../../assets/share.png')}
-            />
-          </Pressable>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ImageBackground
+          imageStyle={{borderRadius: 20}}
+          style={styles.mainImage}
+          source={{uri: mainImage}}>
+          <View style={styles.headerContainer}>
+            <Pressable hitSlop={8} onPress={onBack}>
+              <Image
+                style={styles.iconBack}
+                source={require('../../assets/back.png')}
+              />
+            </Pressable>
+            <Pressable hitSlop={8}>
+              <Image
+                style={styles.iconShare}
+                source={require('../../assets/share.png')}
+              />
+            </Pressable>
+          </View>
+          <View style={styles.footerContainer}>
+            {slicedImage?.map((image, index) => (
+              <Pressable key={image}>
+                <Image style={styles.miniImage} source={{uri: image}} />
+                {diffImage > 0 && index === slicedImage?.length - 1 ? (
+                  <View style={styles.moreImageContainer}>
+                    <Text style={styles.moreImage}>{`+${diffImage}`}</Text>
+                  </View>
+                ) : null}
+              </Pressable>
+            ))}
+          </View>
+        </ImageBackground>
+        <View style={styles.textContainer}>
+          <View>
+            <Text style={styles.title}>{item?.name}</Text>
+            <Text style={styles.city}>{item?.city}</Text>
+          </View>
+          <Text style={styles.price}>{item?.entry_price}</Text>
         </View>
-      </ImageBackground>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/Screen/AttractionDetails/styles.js
+++ src/Screen/AttractionDetails/styles.js
@@ -4,11 +4,14 @@ const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
-    margin: 32,
+    margin: 16,
   },
   mainImage: {
+    flexDirection: 'column',
     width: '100%',
     height: height / 2,
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   iconBack: {
     width: 32,
@@ -23,9 +26,52 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   headerContainer: {
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignContent: 'center',
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    borderRadius: 20,
+    marginBottom: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.35)',
+  },
+  miniImage: {
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    marginHorizontal: 6,
+    margin: 8,
+  },
+  moreImageContainer: {
+    position: 'absolute',
+    top: 8,
+    left: 6,
+    width: 40,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.35)',
+    borderRadius: 8,
+  },
+  moreImage: {
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 20,
+  },
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 20,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'black',
   },
 });
 
